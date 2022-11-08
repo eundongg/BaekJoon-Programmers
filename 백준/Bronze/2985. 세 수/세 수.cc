f924a773ba2ea351ad59eeaa87675beec5699ebf
eundongg <114046224+eundongg@users.noreply.github.com>
2022-11-08 16:16:10 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if (a + b == c)cout << a << '+' << b << '=' << c;
+	else if (a == b + c)cout << a << '=' << b << '+' << c;
+	else if (a - b == c)cout << a << '-' << b << '=' << c;
+	else if (a == b - c)cout << a << '=' << b << '-' << c;
+	else if (a * b == c)cout << a << '*' << b << '=' << c;
+	else if (a == b * c)cout << a << '=' << b << '*' << c;
+	else if (a / b == c)cout << a << '/' << b << '=' << c;
+	else if (a == b / c)cout << a << '=' << b << '/' << c;
+
+	return 0;
+}
