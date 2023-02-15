e4a3f3e46614903385d93db5233d9ddb8fde2404
eundongg <114046224+eundongg@users.noreply.github.com>
2023-02-15 23:41:49 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,37 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-int n,m;
-vector <int> v;
-bool visited[9] = {0, };
-
-void dfs(int cnt){
-    if(cnt == m){
-        for(int i=0;i<m;i++){
-            cout << v[i] << ' ';
-        }
-        cout << '\n';
-        return;
-    }
-    for(int i=1;i<=n;i++){
-        if(!visited[i]){
-            visited[i]=true;
-            v.push_back(i);
-            dfs(cnt+1);
-            v.pop_back();
-            visited[i]=false;
-            
-        }
-    }
-}
+int n, m;
+vector <int> graph;
 int main(){
-    
+
     cin.tie(NULL);
     cout.tie(NULL);
     ios::sync_with_stdio(false);
-    
-    cin >> n>> m;
-    dfs(0);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        graph.push_back(i);
+    }
+
+    do {
+        for (int i = 0; i < m; i++ ) {
+            cout << graph[i] << ' ';
+        }
+        cout << '\n';
+        reverse(graph.begin() + m, graph.end());
+    } while (next_permutation(graph.begin(), graph.end()));
+
     return 0;
-}
+ }
