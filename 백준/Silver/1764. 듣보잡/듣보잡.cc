383bd04538a643fa04cdd70328a282130e74606a
eundongg <114046224+eundongg@users.noreply.github.com>
2023-03-15 12:45:15 +0900

    [Silver IV] Title: 듣보잡, Time: 140 ms, Memory: 37784 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n,m;
+    cin >> n>> m;
+    string unheard[500001];
+    string unseen[500001];
+    vector <string> n_name;
+
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        cin >> unheard[i];
+    }
+    sort(unheard, unheard+n);
+    for(int j=0;j<m;j++){
+        cin >> unseen[j];
+        if(binary_search(unheard, unheard+n,unseen[j])){
+            cnt++;
+            n_name.push_back(unseen[j]);
+        }
+    }
+    sort(n_name.begin(), n_name.end());
+    cout << cnt << '\n';
+
+    for(int i=0;i<n_name.size();i++){
+        cout << n_name[i] << '\n';
+    }
+
+    return 0;
+}
