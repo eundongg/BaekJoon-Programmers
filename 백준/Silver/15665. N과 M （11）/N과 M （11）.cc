139ba5d5033562a0a7f676d55bb569f8ad345a1f
eundongg <114046224+eundongg@users.noreply.github.com>
2023-10-28 13:21:25 +0900

    [Silver II] Title: N과 M (11), Time: 120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n=0, m=0;
+bool visited[9]={0};
+vector <int> nums;
+int arr[9]={0};
+
+
+void combination(int cnt){
+    if(cnt == m){
+        for(int i=0;i<m;i++){
+            cout << nums[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int last = 0;
+    for(int i=1;i<=n;i++){
+        if(arr[i-1] !=last){
+            visited[i] =1;
+            nums.push_back(arr[i-1]);
+            last = nums.back();
+            combination(cnt + 1 );
+            visited[i]=0;
+            nums.pop_back();
+        }
+    }
+}
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n>> m;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+    combination(0);
+
+    return 0;
+}
