f072a7520d7b16d0c8519b3b6effbccb85f7037d
eundongg <114046224+eundongg@users.noreply.github.com>
2025-01-23 21:39:04 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,8 @@
 function solution(my_string, k) {
     var answer = '';
-    for(let i=0;i<k;i++){
-        answer += my_string;
-    }
+    answer += my_string.repeat(k);
+    // for(let i=0;i<k;i++){
+    //     answer += my_string;
+    // }
     return answer;
 }
