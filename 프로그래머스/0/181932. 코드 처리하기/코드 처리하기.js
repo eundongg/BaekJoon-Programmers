ba4f55429994be9b212a81a6b852ea346994ffb9
eundongg <114046224+eundongg@users.noreply.github.com>
2025-01-26 00:16:07 +0900

    [level 0] Title: 코드 처리하기, Time: 37.48 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,14 @@
+function solution(code) {
+    var answer = '';
+    let mode = 0;
+    for(let i=0;i<code.length;i++){
+        if(code[i]==="1"){
+            mode = 1-mode;
+        }else if((mode===0&&i%2==0||mode===1&&i%2===1)){
+            answer+=code[i];
+        }
+        
+    }
+    
+    return answer === ''? "EMPTY": answer;
+}
