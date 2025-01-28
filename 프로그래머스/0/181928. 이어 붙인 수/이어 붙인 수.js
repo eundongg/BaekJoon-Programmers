8ca982c73acffc81933e863fee759c059ee1b51b
eundongg <114046224+eundongg@users.noreply.github.com>
2025-01-28 16:44:41 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,10 +1,17 @@
 function solution(num_list) {
     
-    const {oddSum,evenSum} = num_list.reduce((acc,num)=> {
-        if(num%2==1) acc.oddSum += num;
-        else acc.evenSum += num;
-        return acc;
-    },{oddSum:"",evenSum:""});
+    let oddSum="";
+    let evenSum="";
+    
+    for(let num of num_list){
+        num%2==1? (oddSum+=num):(evenSum+=num);
+    }
+    
+    // const {oddSum,evenSum} = num_list.reduce((acc,num)=> {
+    //     if(num%2==1) acc.oddSum += num;
+    //     else acc.evenSum += num;
+    //     return acc;
+    // },{oddSum:"",evenSum:""});
     
     return Number(oddSum)+Number(evenSum);
 }
