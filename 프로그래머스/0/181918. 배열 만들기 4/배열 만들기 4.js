45891186eba2ccb2c7da8fdc587fd3a6442aa9a1
eundongg <114046224+eundongg@users.noreply.github.com>
2025-02-20 18:36:25 +0900

    [level 0] Title: 배열 만들기 4, Time: 22.96 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -3,17 +3,11 @@ function solution(arr) {
     let i =0;
     
     while(i<arr.length){
-        let len = stk.length;
-        if(len == 0) {
+        if(stk.length == 0 || stk.at(-1) < arr[i]){
             stk.push(arr[i]);
-            i++
+            i++;
         }else{
-            if(stk[len-1]<arr[i]){
-                stk.push(arr[i]);
-                i++;
-            }else{
-                stk.pop();
-            }
+            stk.pop();            
         }         
     }
     
