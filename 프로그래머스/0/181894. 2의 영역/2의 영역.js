4fd1d70531aea09955c938047d3f087bcab8dfb7
eundongg <114046224+eundongg@users.noreply.github.com>
2025-04-01 23:12:52 +0900

    [level 0] Title: 2의 영역, Time: 7.17 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,14 +1,8 @@
 function solution(arr) {
     const idx = arr.map((value,index)=> (value===2? index:-1))
         .filter(index => index!==-1);
-    let newArr = [];
-    if(idx.length===0) return [-1];
-    else{
-        for(let i =0;i<idx.length-1;i++){
-            newArr = newArr.concat(arr.slice(idx[i],idx[i+1]));
-        }
-        newArr.push(arr[idx[idx.length-1]]);
     
-    }
-    return newArr;    
+    if(idx.length===0) return [-1];
+    
+    return arr.slice(idx[0],idx[idx.length-1]+1);    
 }
