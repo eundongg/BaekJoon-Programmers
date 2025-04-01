993fffdfc11e14f4f734c8b16dbbf14b5a2b548a
eundongg <114046224+eundongg@users.noreply.github.com>
2025-04-01 15:30:28 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.02 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,5 +1,4 @@
 function solution(arr, intervals) {
     
-    return [...arr.slice(intervals[0][0],intervals[0][1]+1),
-    ...arr.slice(intervals[1][0],intervals[1][1]+1)];
+    return arr.slice(intervals[0][0],intervals[0][1]+1).concat(arr.slice(intervals[1][0],intervals[1][1]+1));
 }
