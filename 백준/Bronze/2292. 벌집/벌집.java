f4fdf16e23586fe86413c99457a615ea37f90d96
eundongg <114046224+eundongg@users.noreply.github.com>
2026-01-31 00:27:49 +0900

    [Bronze II] Title: 벌집, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());      
+       
+        int cnt =1; // 층수
+        int range = 1; // 해당 층의 최대 숫자 
+        
+        while(range<N){
+            range = range + (6*cnt);
+            cnt++;
+        }
+        System.out.println(cnt);
+        
+    }
+}   
