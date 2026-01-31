657d23c53f6fe2a9618a720a3a9aed4c95dcb649
eundongg <114046224+eundongg@users.noreply.github.com>
2026-01-31 17:41:27 +0900

    [Silver V] Title: 영화감독 숌, Time: 336 ms, Memory: 88996 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int num = 666;
+        int cnt = 1;
+        
+        while(cnt !=N){
+            num++;
+            if(String.valueOf(num).contains("666")){
+                cnt++;
+            }
+        }
+        System.out.print(num);
+    }
+}
